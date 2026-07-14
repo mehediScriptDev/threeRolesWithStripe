6c2e03cbdf08e6936c5a0444010f113697451d18
mehedi <mehedi25122003@gmail.com>
2026-07-14 22:46:20 +0600

    server file added

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import { Application, Request, Response } from "express";
+import express from "express";
+
+const app: Application = express();
+
+app.get("/", (req:Request, res:Response) => {
+    res.send("Hello, World!");
+});
+
+export default app;
